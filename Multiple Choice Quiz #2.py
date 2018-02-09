921d798fe9569a58ffa20b268e734e0e404dab71
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-02-08 21:11:43 -0500

    Update Multiple Choice Quiz #2.py

--- Multiple Choice Quiz #2.py
+++ Multiple Choice Quiz #2.py
@@ -154,7 +154,7 @@ while True:
   #percentage score
   final_score=(score*100)/6
   print("This is a score of" + str(final_score),"percent")
-  User== ("Do you want to retake the quiz? y/n?")
+  User= ("Do you want to retake the quiz? y/n?")
   if User==True:
     print("retake quiz")
   else:
